5d62c74e2a09265c70f3a16c24c93fe0d2d2e532
AritriBiswas <aritribiswas07@gmail.com>
2022-07-15 18:36:52 +0530

    messaging feature added

---
+++ src/Login.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import "./Login.css";
+import { Button } from "@material-ui/core";
+import { auth, provider } from "./firebase";
+import { useStateValue } from "./StateProvider";
+import { actionTypes } from "./reducer";
+
+const Login = () => {
+    const [state, dispatch] = useStateValue();
+
+    const signIn = () => {
+        auth.signInWithPopup(provider)
+            .then((result) => {
+                dispatch({
+                    type: actionTypes.SET_USER,
+                    user: result.user,
+                });
+            })
+            .catch((error) => {
+                alert(error.message);
+            });
+    };
+
+    return (
+        <body className="bg">
+        <div className="login" >
+            <div className="login__container">
+                <img
+                    src="https://a.slack-edge.com/80588/marketing/img/icons/icon_slack_hash_colored.png"
+                    alt=""
+                />
+
+                <h1>Sign in</h1>
+                <p>Slack</p>
+                <Button onClick={signIn}>Sign in with Google</Button>
+            </div>
+        </div >
+        </body>
+    )
+}
+
+export default Login
+
+
+
+
